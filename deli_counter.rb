6e002733f60bd659e605819907b5ce1b2fa449ad
Min Kim <mk4555@nyu.edu>
2017-10-22 16:13:48 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -22,6 +22,6 @@ def now_serving(array)
   if array.size == 0
     puts "There is nobody waiting to be served!"
   else
-    puts "Currently serving #{array.pop}."
+    puts "Currently serving #{array.shift}."
   end
 end
