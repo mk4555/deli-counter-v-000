4717b9852c5df41b5443d0da20323df0e74adecd
Min Kim <mk4555@nyu.edu>
2017-10-22 16:02:50 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,7 +1,10 @@
 # Write your code here.
 def line(array)
+  counter = 0
   if array.empty?
     puts "The line is currently empty."
   else
     print "The line is currently :"
+    array.each do |a|
+      print ""
 end
